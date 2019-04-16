8b8a43b2e58d1022318ec2e82e1fff07d48173c7
JamesPeralta <jamesperalta35@gmail.com>
2019-04-15 22:24:08 -0600

    Fix all helper classes

--- Neural-Network/Iteration1/GymnoDataGenerator_i1.py
+++ Neural-Network/Iteration1/GymnoDataGenerator_i1.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from tensorflow.python.keras.utils.data_utils import Sequence
 
+
 class DataGenerator(Sequence):
     # Initialization
     def __init__(self, list_IDs, labels, data_location, batch_size=32, dim=(32,32,32), n_classes=2, shuffle=True):
@@ -53,33 +54,3 @@ class DataGenerator(Sequence):
         X, y = self.__data_generation(self.list_IDs[index*self.batch_size:(index+1)*self.batch_size])
 
         return X, y
-
-
-    # Plot multiple images with matplotlib in a single figure. Titles can be given optionally as second argument.
-    def show_images(self, images, cols = 1, titles = None):
-        """Display a list of images in a single figure with matplotlib.
-        
-        Parameters
-        ---------
-        images: List of np.arrays compatible with plt.imshow.
-        
-        cols (Default = 1): Number of columns in figure (number of rows is 
-                            set to np.ceil(n_images/float(cols))).
-        
-        titles: List of titles corresponding to each image. Must have
-                the same length as titles.
-        """
-        assert((titles is None)or (len(images) == len(titles)))
-        n_images = len(images)
-        if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
-        fig = plt.figure()
-        for n, (image, title) in enumerate(zip(images, titles)):
-            a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
-            if image.ndim == 2:
-                plt.gray()
-            plt.imshow(image)
-            a.set_title(title)
-        fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
-        plt.show()
-
-        
--- Neural-Network/Iteration1/HelperFunctions_i1.py
+++ Neural-Network/Iteration1/HelperFunctions_i1.py
@@ -199,19 +199,18 @@ def readPartitionCsv(source):
     return partition
 
   
-def show_images(self, images, cols = 1, titles = None):
-	"""
+def show_images(images, cols=1, titles=None):
+    """
     Display a list of images in a single figure with matplotlib.
-        
+
     Parameters
     ---------
     images: List of np.arrays compatible with plt.imshow.
-        
+
     cols (Default = 1): Number of columns in figure (number of rows is set to np.ceil(n_images/float(cols))).
-        
+
     titles: List of titles corresponding to each image. Must have the same length as titles.
     """
-    
     assert((titles is None)or (len(images) == len(titles)))
     n_images = len(images)
     if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
