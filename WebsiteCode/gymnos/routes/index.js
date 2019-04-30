b56d785bf5d4f33de245d36e60bda15bd7890508
issackj1 <issack.john1@ucalgary.ca>
2019-04-30 01:41:30 -0600

    attempting to laod model to browser

--- WebsiteCode/gymnos/routes/index.js
+++ WebsiteCode/gymnos/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+let tf = require('tensorflowjs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+async function getModel(){
+  const model = await tf.loadModel('https://gymnos/tfjs_files/model.json');
+}
 
 module.exports = router;
