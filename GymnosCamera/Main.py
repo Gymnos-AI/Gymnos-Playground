6a80b86446c21855ebdc72b66cce01ba168077f2
jamesperalta <jamesperalta35@gmail.com>
2019-06-06 20:49:00 -0600

    Add UsbCameraMain code

--- GymnosCamera/Main.py
+++ GymnosCamera/Main.py
@@ -1,5 +1,30 @@
-import PiCameraMain as piCode
+camera_type = 'picamera'
 
-piCam = piCode.PiCameraMain()
+# import the necessary packages
+try:
+    import PiCameraMain as piCam
+except ImportError:
+    print('No PiCamera available, setting as usb device')
+    camera_type = 'usb'
+    import GymnosCamera.UsbCameraMain as usbCam
 
-piCam.run_loop()
+import argparse
+
+# Select camera type (if user enters --usbcam when calling this script,
+# a USB webcam will be used)
+parser = argparse.ArgumentParser()
+parser.add_argument('--usbcam', help='Use a USB webcam instead of picamera',
+                    action='store_true')
+args = parser.parse_args()
+if args.usbcam:
+    camera_type = 'usb'
+
+
+# Run loop of either PiCamera or USB
+if camera_type == 'picamera':
+    piCam = piCam.PiCameraMain()
+
+    piCam.run_loop()
+else:
+    usbCam = usbCam.UsbCameraMain()
+    usbCam.run_loop()
--- GymnosCamera/UsbCameraMain.py
+++ GymnosCamera/UsbCameraMain.py
@@ -1,19 +1,23 @@
 # Continuously capture frames and perform object detection on them
 import cv2
 
-while True:
-    IM_WIDTH = 640
-    IM_HEIGHT = 480
-    camera = cv2.VideoCapture(0)
 
-    # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
-    # i.e. a single-column array, where each item in the column has the pixel RGB value
-    ret, image = camera.read()
+class UsbCameraMain:
+    def __init__(self):
+        # initialize the camera
+        self.camera = cv2.VideoCapture(0)
 
-    # show the output images
-    cv2.imshow("Video Feed", image)
+    def run_loop(self):
+        """
+        This main loop will grab frames the camera and print it onto the screen
+        """
+        # capture frames from the camera
+        while True:
+            ret, image = self.camera.read()
 
+            # show the output images
+            cv2.imshow("Video Feed", image)
 
-    # Press 'q' to quit
-    if cv2.waitKey(1) == ord('q'):
-        break
+            # Press 'q' to quit
+            if cv2.waitKey(1) == ord('q'):
+                break
