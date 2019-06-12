8e16cf6c73edea963345e8ae96c9f87a7738873a
Sunny Nagam <sunnynagam1@gmail.com>
2019-06-11 20:49:35 -0600

    Added station overlap tracking and timing

--- GymnosCamera/UsbCameraMain.py
+++ GymnosCamera/UsbCameraMain.py
@@ -3,13 +3,27 @@ import cv2
 import Predictors
 import numpy as np
 
+# TODO: Fetch this from a json file, currently being mocked with this dict
+CAMERA_MACHINES = {
+    "Main Floor" : [
+        [0.1, 0.1, 0.3, 0.8, "Squat-Rack"],
+        [0.75, 0.2, 0.95, 0.8, "Bench"]
+    ]
+}
+
+CURRENT_LOCATION = "Main Floor"
+iou_threshold = 0.01  
 
 class UsbCameraMain:
     def __init__(self):
         # initialize the camera
         self.camera = cv2.VideoCapture(0)
-        self.camera_height = 128
-        self.camera_width = 128
+        self.camera_height = 512
+        self.camera_width = 512
+        self.time_used = {
+            "Squat-Rack":0,
+            "Bench":0
+        }
 
         # initialize the Predictors
         self.predictor = Predictors.Predictors('YOLOV3')
@@ -18,9 +32,6 @@ class UsbCameraMain:
         """
         This main loop will grab frames the camera and print it onto the screen
         """
-        # capture frames from the camera
-        count = 0
-        # list_of_coords = [(0, 0, 0, 0)]
         while True:
             ret, image = self.camera.read()
             image = cv2.resize(image, (self.camera_height, self.camera_width))
@@ -31,12 +42,52 @@ class UsbCameraMain:
             for (topX, leftY, bottomX, rightY) in list_of_coords:
                 cv2.rectangle(image, (topX, leftY), (bottomX, rightY), (0, 0, 255), 2)
 
-            image = np.asarray(image)
-            image = cv2.resize(image, (416, 416))
-            cv2.imshow("Video Feed", image)
+            list_of_stations = CAMERA_MACHINES[CURRENT_LOCATION]
+            font = cv2.FONT_HERSHEY_SIMPLEX
 
-            count = count + 1
+            for (a, b, c, d, name) in list_of_stations:
+                w, h = self.camera_width, self.camera_height
+                topX, leftY, bottomX, rightY = (int)(a*w), (int)(b*h), (int)(c*w), (int)(d*h)
+
+                # display machine areas
+                cv2.rectangle(image, (topX, leftY), (bottomX, rightY), (0, 255, 0), 1)
+                cv2.putText(image,name + " " + str(self.time_used[name] / 30) + "s",(topX,(int)((leftY+rightY)/2)), font, 1,(0,255,0),2,cv2.LINE_AA)
+
+                # calculate iou for boxes
+                for (HtopX, HleftY, HbottomX, HrightY) in list_of_coords:
+                    if calculate_iou((topX, leftY, bottomX, rightY), (HtopX, HleftY, HbottomX, HrightY)) > iou_threshold:
+                        # Count seconds somebody is in the machine
+                        cv2.rectangle(image, (topX, leftY), (bottomX, rightY), (0, 255, 0), 3)
+                        self.time_used[name] = self.time_used[name] + 1
+
+            image = np.asarray(image)
+            image = cv2.resize(image, (900, 900))
+            cv2.imshow("Video Feed", image)
 
             # Press 'q' to quit
             if cv2.waitKey(1) == ord('q'):
                 break
+
+def calculate_iou(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+ 
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+ 
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+ 
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+ 
+    # return the intersection over union value
+    #print(iou)
+    return iou
--- GymnosCamera/YoloNetwork/YoloV3.py
+++ GymnosCamera/YoloNetwork/YoloV3.py
@@ -11,7 +11,7 @@ from keras import backend as K
 from keras.models import load_model
 from keras.layers import Input
 
-from model import yolo_eval, yolo_body, tiny_yolo_body
+from .model import yolo_eval, yolo_body, tiny_yolo_body
 import os
 from keras.utils import multi_gpu_model
 
@@ -102,7 +102,7 @@ class YOLO(object):
 
         image_data = np.array(image, dtype='float32')
 
-        print(image_data.shape)
+        #print(image_data.shape)
         (image_height, image_width, channels) = image_data.shape
 
         image_data /= 255.
@@ -132,11 +132,11 @@ class YOLO(object):
                 left = max(0, np.floor(left + 0.5).astype('int32'))
                 bottom = min(image_height, np.floor(bottom + 0.5).astype('int32'))
                 right = min(image_width, np.floor(right + 0.5).astype('int32'))
-                print(label, (left, top), (right, bottom))
+                #print(label, (left, top), (right, bottom))
                 list_of_coords.append((left + i, top + i, right - i, bottom - i))
 
         end = timer()
-        print(end - start)
+        #print(end - start)
         return list_of_coords
 
 
